33fa7b95c9c594b9d937947e48c0df3ec9e26691
Nadia Sultana <nadiasultana8899@gmail.com>
2025-05-04 17:05:23 -0400

    backend completed

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const app = require('./src/app');
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8000;
 app.listen(port, () => {
     console.log(`Server running on http://localhost:${port}`);
 });
